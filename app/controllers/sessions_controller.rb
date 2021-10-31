6e78d335d5485f55a67fc838ef7d659ce6e57239
vgoylo <y_draiv.by@mail.ru>
2021-10-31 11:33:33 +0300

    Fix login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,12 @@ class SessionsController < ApplicationController
   def new; end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user&.authenticate(params[:session][:password])
-      log_in user
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_back_or user
+    @user = User.find_by(email: params[:session][:email].downcase)
+
+    if @user&.authenticate(params[:session][:password])
+      log_in @user
+      flash[:success] = 'Welcome to the Sample App!'
+      redirect_to @user
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
