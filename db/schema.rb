989357f9bc9ec11da7607df17218c8b4e1a10df7
vgoylo <y_draiv.by@mail.ru>
2021-11-01 22:34:10 +0300

    Add password resets & email configuration

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_211_031_090_734) do
+ActiveRecord::Schema.define(version: 20_211_101_185_247) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 20_211_031_090_734) do
     t.boolean 'activated', default: false
     t.datetime 'activated_at'
     t.integer 'role', default: 0
+    t.string 'reset_digest'
+    t.datetime 'reset_sent_at'
     t.index ['email'], name: 'index_users_on_email', unique: true
   end
 end
